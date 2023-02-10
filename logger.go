2ee7f44e3f0175bb9e754f5a268d0e1ac3ca7921
samithiwat <samithiwat2544@gmail.com>
2023-02-10 23:09:27 +0700

    feat: create logger lib

---
+++ logger.go
@@ -0,0 +1,124 @@
+package gosdk
+
+import (
+	"github.com/2110336-2565-2/sec3-group15-cu-freelance-library/pkg/log"
+	"github.com/2110336-2565-2/sec3-group15-cu-freelance-library/pkg/sentry"
+	"go.uber.org/zap"
+	"time"
+)
+
+type Logger struct {
+	keyword     map[string]any
+	level       log.Level
+	serviceName string
+	err         error
+	*zap.Logger
+	sentryService sentry.Sentry
+}
+
+func NewLogger(serviceName string) *Logger {
+	lg, _ := zap.NewProduction()
+
+	keyword := map[string]any{}
+
+	return &Logger{
+		keyword,
+		log.Unknown,
+		serviceName,
+		nil,
+		lg,
+		nil,
+	}
+}
+func (s *Logger) SetSentryDSN(dsn string) error {
+	sentrySrv, err := sentry.NewService(dsn)
+	if err != nil {
+		return err
+	}
+
+	s.sentryService = sentrySrv
+	return nil
+}
+
+func (s *Logger) SetName(name string) {
+	s.serviceName = name
+}
+
+func (s *Logger) Info() *Logger {
+	s.level = log.Info
+	return s
+}
+
+func (s *Logger) Error(err error) *Logger {
+	s.level = log.Error
+	return s
+}
+
+func (s *Logger) Warn() *Logger {
+	s.level = log.Warn
+	return s
+}
+
+func (s *Logger) Debug(msg string, options ...interface{}) *Logger {
+	s.level = log.Debug
+	return s
+}
+
+func (s *Logger) Fatal(msg string, options ...interface{}) *Logger {
+	s.level = log.Fatal
+	return s
+}
+
+func (s *Logger) Keyword(key string, val any) *Logger {
+	s.keyword[key] = val
+	return s
+}
+
+func (s *Logger) Msg(msg string) {
+	defer s.Logger.Sync()
+
+	//keywordList := []interface{}{msg}
+	var keywordList []zap.Field
+	s.createKeywordList(&keywordList)
+
+	switch s.level {
+	case log.Debug:
+		s.Logger.Debug(msg, keywordList...)
+	case log.Info:
+		s.Logger.Info(msg, keywordList...)
+	case log.Warn:
+		s.Logger.Warn(msg, keywordList...)
+	case log.Error:
+		if s.sentryService != nil {
+			s.sentryService.CaptureException(s.err)
+		}
+		s.Logger.Error(msg, keywordList...)
+	case log.Fatal:
+		if s.sentryService != nil {
+			s.sentryService.CaptureException(s.err)
+		}
+		s.Logger.Fatal(msg, keywordList...)
+	default:
+		return
+	}
+}
+
+func (s *Logger) createKeywordList(keywords *[]zap.Field) {
+	*keywords = append(*keywords, zap.String("service", s.serviceName))
+
+	if s.err != nil {
+		*keywords = append(*keywords, zap.Error(s.err))
+	}
+
+	for k, v := range s.keyword {
+		switch v.(type) {
+		case string:
+			*keywords = append(*keywords, zap.String(k, v.(string)))
+		case int:
+			*keywords = append(*keywords, zap.Int(k, v.(int)))
+		case time.Duration:
+			*keywords = append(*keywords, zap.Duration(k, v.(time.Duration)))
+		}
+
+	}
+}
--- logger.service.go
+++
@@ -1,9 +0,0 @@
-package loggersdk
-
-type Service interface {
-	SetName(name string)
-	Info(msg string, options ...interface{})
-	Error(msg string, err error, options ...interface{})
-	Debug(msg string, options ...interface{})
-	Fatal(msg string, options ...interface{})
-}
