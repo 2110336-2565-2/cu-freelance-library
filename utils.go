947020b9ed22926e16a0960cf2a04c02ac155a26
samithiwat <samithiwat2544@gmail.com>
2023-02-12 15:37:40 +0700

    feat: add utils functions

--- utils.go
+++ utils.go
@@ -3,10 +3,35 @@ package gosdk
 import (
 	"github.com/pkg/errors"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+func IsExisted(e map[string]struct{}, key string) bool {
+	_, ok := e[key]
+	if ok {
+		return true
+	}
+	return false
+}
+
+func MergeStringSlice(s1 []string, sList ...[]string) []string {
+	for _, s := range sList {
+		s1 = append(s1, s...)
+	}
+	return s1
+}
+
+func TrimInList(word string, sep string, trimList map[string]struct{}) string {
+	splitWordList := strings.Split(word, sep)
+	if _, ok := trimList[splitWordList[1]]; !ok {
+		return word
+	}
+
+	return strings.TrimPrefix(word, sep+splitWordList[1])
+}
 
 func BoolAdr(b bool) *bool {
 	return &b
--- utils_test.go
+++ utils_test.go
@@ -55,3 +55,59 @@ func testCalStudyYearInvalidInput(t *testing.T, sid string) {
 	assert.Equal(t, actual, "")
 	assert.Equal(t, want, err.Error())
 }
+
+func (t *UtilTest) TestIsExistedTrue() {
+	e := map[string]struct{}{
+		"A": {},
+		"B": {},
+	}
+
+	ok := IsExisted(e, "A")
+
+	assert.True(t.T(), ok)
+}
+
+func (t *UtilTest) TestIsExistedFalse() {
+	e := map[string]struct{}{
+		"A": {},
+		"B": {},
+	}
+
+	ok := IsExisted(e, "C")
+
+	assert.False(t.T(), ok)
+}
+
+func (t *UtilTest) TestMergeStringSlice() {
+	var nilSlice []string
+
+	testMergeStringSlice(t.T(), []string{"1"}, []string{"2"}, []string{"1", "2"})
+	testMergeStringSlice(t.T(), []string{"1"}, []string{"2", "3"}, []string{"1", "2", "3"})
+	testMergeStringSlice(t.T(), []string{}, []string{"2"}, []string{"2"})
+	testMergeStringSlice(t.T(), []string{"1"}, []string{}, []string{"1"})
+	testMergeStringSlice(t.T(), nilSlice, nilSlice, nilSlice)
+}
+
+func testMergeStringSlice(t *testing.T, s1 []string, s2 []string, want []string) {
+	actual := MergeStringSlice(s1, s2)
+
+	assert.Equal(t, want, actual)
+}
+
+func (t *UtilTest) TestTrimInList() {
+	trimList := map[string]struct{}{
+		"v1": {},
+		"v2": {},
+	}
+
+	testTrimInList(t.T(), "/path/1", "/v1/path/1", "/", trimList)
+	testTrimInList(t.T(), "/path/something/1", "/v2/path/something/1", "/", trimList)
+	testTrimInList(t.T(), "/exclude/1", "/v1/exclude/1", "/", trimList)
+	testTrimInList(t.T(), "/path/1", "/path/1", "/", trimList)
+}
+
+func testTrimInList(t *testing.T, want string, word string, sep string, trimList map[string]struct{}) {
+	actual := TrimInList(word, sep, trimList)
+
+	assert.Equal(t, want, actual)
+}
