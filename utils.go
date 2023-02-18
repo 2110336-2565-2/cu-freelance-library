8b4849a87524ca9756c47d56cb41d3d579d81a83
samithiwat <samithiwat2544@gmail.com>
2023-02-18 18:04:18 +0700

    feat: create lang utils

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package gosdk
 
 import (
+	"github.com/pemistahl/lingua-go"
 	"github.com/pkg/errors"
 	"strconv"
 	"strings"
@@ -8,6 +9,32 @@ import (
 
 	"github.com/google/uuid"
 )
+
+type LangUtil interface {
+	CheckLang(string) lingua.Language
+}
+
+type langUtil struct {
+	detector lingua.LanguageDetector
+}
+
+func NewLangUtil(languages []lingua.Language) LangUtil {
+	detector := lingua.NewLanguageDetectorBuilder().
+		FromLanguages(languages...).
+		Build()
+
+	return &langUtil{
+		detector: detector,
+	}
+}
+
+func (u *langUtil) CheckLang(text string) lingua.Language {
+	if language, exists := u.detector.DetectLanguageOf(text); exists {
+		return language
+	}
+
+	return lingua.Unknown
+}
 
 func IsExisted(e map[string]struct{}, key string) bool {
 	_, ok := e[key]
