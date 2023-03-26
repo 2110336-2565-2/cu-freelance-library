6afbda7a13444326e08341a01581aa50ba685dcf
Chaiyapluek Musakul <chaiyapluekmus@gmail.com>
2023-03-27 00:05:22 +0700

    feat: add delete avater and port image

--- pkg/pb/file_grpc.pb.go
+++ pkg/pb/file_grpc.pb.go
@@ -23,10 +23,12 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type FileServiceClient interface {
 	UploadAvatarImage(ctx context.Context, in *UploadAvatarImageRequest, opts ...grpc.CallOption) (*UploadAvatarImageResponse, error)
-	UploadPortfolioImage(ctx context.Context, in *UploadPortfolioImageRequest, opts ...grpc.CallOption) (*UploadPortfolioImageResponse, error)
 	GetUserAvatar(ctx context.Context, in *GetUserAvatarRequest, opts ...grpc.CallOption) (*UserAvatarResponse, error)
+	DeleteAvater(ctx context.Context, in *DeleteAvaterRequest, opts ...grpc.CallOption) (*DeleteAvaterResponse, error)
+	UploadPortfolioImage(ctx context.Context, in *UploadPortfolioImageRequest, opts ...grpc.CallOption) (*UploadPortfolioImageResponse, error)
 	GetPortThumbnail(ctx context.Context, in *GetPortThumbnailRequest, opts ...grpc.CallOption) (*PortThumbnailResponse, error)
 	GetAllPortImage(ctx context.Context, in *GetAllPortImageRequest, opts ...grpc.CallOption) (*AllPortImageResponse, error)
+	DeletePortfolioImage(ctx context.Context, in *DeletePortfolioImageRequest, opts ...grpc.CallOption) (*DeletePortfolioImageResponse, error)
 }
 
 type fileServiceClient struct {
@@ -46,18 +48,27 @@ func (c *fileServiceClient) UploadAvatarImage(ctx context.Context, in *UploadAva
 	return out, nil
 }
 
-func (c *fileServiceClient) UploadPortfolioImage(ctx context.Context, in *UploadPortfolioImageRequest, opts ...grpc.CallOption) (*UploadPortfolioImageResponse, error) {
-	out := new(UploadPortfolioImageResponse)
-	err := c.cc.Invoke(ctx, "/file.FileService/UploadPortfolioImage", in, out, opts...)
+func (c *fileServiceClient) GetUserAvatar(ctx context.Context, in *GetUserAvatarRequest, opts ...grpc.CallOption) (*UserAvatarResponse, error) {
+	out := new(UserAvatarResponse)
+	err := c.cc.Invoke(ctx, "/file.FileService/GetUserAvatar", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *fileServiceClient) GetUserAvatar(ctx context.Context, in *GetUserAvatarRequest, opts ...grpc.CallOption) (*UserAvatarResponse, error) {
-	out := new(UserAvatarResponse)
-	err := c.cc.Invoke(ctx, "/file.FileService/GetUserAvatar", in, out, opts...)
+func (c *fileServiceClient) DeleteAvater(ctx context.Context, in *DeleteAvaterRequest, opts ...grpc.CallOption) (*DeleteAvaterResponse, error) {
+	out := new(DeleteAvaterResponse)
+	err := c.cc.Invoke(ctx, "/file.FileService/DeleteAvater", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *fileServiceClient) UploadPortfolioImage(ctx context.Context, in *UploadPortfolioImageRequest, opts ...grpc.CallOption) (*UploadPortfolioImageResponse, error) {
+	out := new(UploadPortfolioImageResponse)
+	err := c.cc.Invoke(ctx, "/file.FileService/UploadPortfolioImage", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -82,15 +93,26 @@ func (c *fileServiceClient) GetAllPortImage(ctx context.Context, in *GetAllPortI
 	return out, nil
 }
 
+func (c *fileServiceClient) DeletePortfolioImage(ctx context.Context, in *DeletePortfolioImageRequest, opts ...grpc.CallOption) (*DeletePortfolioImageResponse, error) {
+	out := new(DeletePortfolioImageResponse)
+	err := c.cc.Invoke(ctx, "/file.FileService/DeletePortfolioImage", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FileServiceServer is the server API for FileService service.
 // All implementations should embed UnimplementedFileServiceServer
 // for forward compatibility
 type FileServiceServer interface {
 	UploadAvatarImage(context.Context, *UploadAvatarImageRequest) (*UploadAvatarImageResponse, error)
-	UploadPortfolioImage(context.Context, *UploadPortfolioImageRequest) (*UploadPortfolioImageResponse, error)
 	GetUserAvatar(context.Context, *GetUserAvatarRequest) (*UserAvatarResponse, error)
+	DeleteAvater(context.Context, *DeleteAvaterRequest) (*DeleteAvaterResponse, error)
+	UploadPortfolioImage(context.Context, *UploadPortfolioImageRequest) (*UploadPortfolioImageResponse, error)
 	GetPortThumbnail(context.Context, *GetPortThumbnailRequest) (*PortThumbnailResponse, error)
 	GetAllPortImage(context.Context, *GetAllPortImageRequest) (*AllPortImageResponse, error)
+	DeletePortfolioImage(context.Context, *DeletePortfolioImageRequest) (*DeletePortfolioImageResponse, error)
 }
 
 // UnimplementedFileServiceServer should be embedded to have forward compatible implementations.
@@ -100,17 +122,23 @@ type UnimplementedFileServiceServer struct {
 func (UnimplementedFileServiceServer) UploadAvatarImage(context.Context, *UploadAvatarImageRequest) (*UploadAvatarImageResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UploadAvatarImage not implemented")
 }
-func (UnimplementedFileServiceServer) UploadPortfolioImage(context.Context, *UploadPortfolioImageRequest) (*UploadPortfolioImageResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UploadPortfolioImage not implemented")
-}
 func (UnimplementedFileServiceServer) GetUserAvatar(context.Context, *GetUserAvatarRequest) (*UserAvatarResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetUserAvatar not implemented")
+}
+func (UnimplementedFileServiceServer) DeleteAvater(context.Context, *DeleteAvaterRequest) (*DeleteAvaterResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteAvater not implemented")
+}
+func (UnimplementedFileServiceServer) UploadPortfolioImage(context.Context, *UploadPortfolioImageRequest) (*UploadPortfolioImageResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UploadPortfolioImage not implemented")
 }
 func (UnimplementedFileServiceServer) GetPortThumbnail(context.Context, *GetPortThumbnailRequest) (*PortThumbnailResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetPortThumbnail not implemented")
 }
 func (UnimplementedFileServiceServer) GetAllPortImage(context.Context, *GetAllPortImageRequest) (*AllPortImageResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAllPortImage not implemented")
+}
+func (UnimplementedFileServiceServer) DeletePortfolioImage(context.Context, *DeletePortfolioImageRequest) (*DeletePortfolioImageResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeletePortfolioImage not implemented")
 }
 
 // UnsafeFileServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -142,24 +170,6 @@ func _FileService_UploadAvatarImage_Handler(srv interface{}, ctx context.Context
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FileService_UploadPortfolioImage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UploadPortfolioImageRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(FileServiceServer).UploadPortfolioImage(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/file.FileService/UploadPortfolioImage",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FileServiceServer).UploadPortfolioImage(ctx, req.(*UploadPortfolioImageRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 func _FileService_GetUserAvatar_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetUserAvatarRequest)
 	if err := dec(in); err != nil {
@@ -174,6 +184,42 @@ func _FileService_GetUserAvatar_Handler(srv interface{}, ctx context.Context, de
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(FileServiceServer).GetUserAvatar(ctx, req.(*GetUserAvatarRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _FileService_DeleteAvater_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteAvaterRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FileServiceServer).DeleteAvater(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/file.FileService/DeleteAvater",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FileServiceServer).DeleteAvater(ctx, req.(*DeleteAvaterRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _FileService_UploadPortfolioImage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UploadPortfolioImageRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FileServiceServer).UploadPortfolioImage(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/file.FileService/UploadPortfolioImage",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FileServiceServer).UploadPortfolioImage(ctx, req.(*UploadPortfolioImageRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -214,6 +260,24 @@ func _FileService_GetAllPortImage_Handler(srv interface{}, ctx context.Context, 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _FileService_DeletePortfolioImage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeletePortfolioImageRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FileServiceServer).DeletePortfolioImage(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/file.FileService/DeletePortfolioImage",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FileServiceServer).DeletePortfolioImage(ctx, req.(*DeletePortfolioImageRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // FileService_ServiceDesc is the grpc.ServiceDesc for FileService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -226,12 +290,16 @@ var FileService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _FileService_UploadAvatarImage_Handler,
 		},
 		{
-			MethodName: "UploadPortfolioImage",
-			Handler:    _FileService_UploadPortfolioImage_Handler,
-		},
-		{
 			MethodName: "GetUserAvatar",
 			Handler:    _FileService_GetUserAvatar_Handler,
+		},
+		{
+			MethodName: "DeleteAvater",
+			Handler:    _FileService_DeleteAvater_Handler,
+		},
+		{
+			MethodName: "UploadPortfolioImage",
+			Handler:    _FileService_UploadPortfolioImage_Handler,
 		},
 		{
 			MethodName: "GetPortThumbnail",
@@ -240,6 +308,10 @@ var FileService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetAllPortImage",
 			Handler:    _FileService_GetAllPortImage_Handler,
+		},
+		{
+			MethodName: "DeletePortfolioImage",
+			Handler:    _FileService_DeletePortfolioImage_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
